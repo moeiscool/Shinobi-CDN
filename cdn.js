0f8f3e09653dd162fd535019fe097f1bdbfe3c8f
Moe <github@m03.ca>
2017-12-14 15:32:37 -0800

    allow opening content on any page

--- cdn.js
+++ cdn.js
@@ -16,10 +16,16 @@ var express = require('express');
 var app = express();
 var http = require('http');
 var server = http.createServer(app);
+
 server.listen(80,function(){
     console.log('CDN on Port 80');
 });
 app.enable('trust proxy');
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 app.use('/',express.static(__dirname + '/files'));
 app.get('/', function (req,res){
     res.sendFile(__dirname+'/index.html');
